36adb0542768f0ea2d194badfb7a223604719596
NosukeSinjo <ayataka3270505@gmail.com>
2023-08-03 16:10:54 +0900

    create_merchant
     加盟店を作ったよ

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,22 @@
-class MerchantsController < ApllicationController
-    def new; end
-    def show; end
-    def create; end
+class MerchantsController < ApplicationController
+  def new; end
+
+  def show
+    @merchant = current_merchant
+  end
+  
+  def create
+    @merchant = Merchant.new(merchant_params)
+    if @merchant.save!
+      flash[:notice] = "登録が完了しました"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def merchant_params
+    params.require(:merchant).permit(:merchant_name)
+  end
 end
